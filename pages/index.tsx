838fccc3cd191c032c240b3fea603de73c126c33
Bediaf Akram <88899765+Akram-Quark@users.noreply.github.com>
2022-05-18 22:57:48 +0100

    +sanity posts setup and connect to front-end done

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
-
-const Home: NextPage = () => {
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
+interface Props {
+  posts: [Post]
+}
+const Home: NextPage<Props> = ({ posts }: Props) => {
+  console.log(posts)
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -12,9 +17,29 @@ const Home: NextPage = () => {
       </Head>
       {/*hero section */}
       <Header />
+      {/*posts section */}
+
       <Hero />
     </div>
   )
 }
 
 export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    author->{
+      name,
+      image
+    },
+    mainImage,
+    slug
+  }`
+  const posts = await sanityClient.fetch(query)
+  return {
+    props: {
+      posts,
+    },
+  }
+}
---
+++ sanity.js
@@ -0,0 +1,13 @@
+import {
+  createImageUelBuilder,
+  createCurrentUserHook,
+  createClient,
+} from 'next-sanity'
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2022-05-18',
+  useCdn: process.env.NODE_ENV === 'production',
+}
+export const sanityClient = createClient(config)
+export const urlFor = (source) => createImageUelBuilder(config).image(source)
